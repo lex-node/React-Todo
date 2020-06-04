4ffee95c0106e1b73c7a0b944906e35444665169
triggering-event <gabriel.j.shapiro@gmail.com>
2020-06-04 19:34:06 -0400

    fix typo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 class Todo extends React.Component {
     render() {
         return (
@@ -8,7 +7,7 @@ class Todo extends React.Component {
                 {this.props.task.completed ? <h2 style={{textDecoration: "line-through"}}>{this.props.task.task}</h2> :
                     <h2>{this.props.task.task}</h2>}
                 <p>Completed? {this.props.task.completed ? "Yes" : "Not yet"}</p>
-                <button id={this.props.task.task} onClick={(event) => {
+                <button id={this.props.task} onClick={(event) => {
                     event.persist();
                     this.props.handleTaskCompletion(event)
                 }}>Mark Completed
